0f935a4bf4b87b3631f1e7bef4fb66bfc8755c72
Jev05 <167942751+Jev05@users.noreply.github.com>
2025-10-25 12:43:00 -0600

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";      // usuario por defecto en XAMPP
 $password = "";          // contraseña vacía por defecto
-$dbname = "pruebamty";      // tu base de datos
+$dbname = "banorte";      // tu base de datos
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
